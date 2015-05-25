7486adf0b9b6583e3cbbdd74f41e4212b5038f28
Jack Sneed <sneed.jack@gmail.com>
2015-05-25 03:20:39 -0700

    Fix bug

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -6,7 +6,7 @@ import re
 main_page_head = '''
 <head>
     <meta charset="utf-8">
-    <title>Jack's Favorite Films</title>
+    <title>Jack's Favorite Movies!</title>
 
     <!-- Bootstrap 3 -->
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
@@ -99,6 +99,22 @@ main_page_content = '''
         </div>
       </div>
     </div>
+    
+    <!-- Main Page Content -->
+    <div class="container">
+      <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">Jack's Favorite Movies</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      {movie_tiles}
+    </div>
+  </body>
+</html>
 '''
 
 # A single movie entry html template
@@ -109,7 +125,6 @@ movie_tile_content = '''
     <p>{movie_storyline}</p>
 </div>
 '''
-
 
 def create_movie_tiles_content(movies):
     # The HTML content for this section of the page
@@ -129,18 +144,17 @@ def create_movie_tiles_content(movies):
         )
     return content
 
-
 def open_movies_page(movies):
-    # Create or overwrite the output file
-    output_file = open('fresh_tomatoes.html', 'w')
+  # Create or overwrite the output file
+  output_file = open('fresh_tomatoes.html', 'w')
 
-    # Replace the placeholder for the movie tiles with the actual dynamically generated content
-    rendered_content = main_page_content.format(movie_tiles=create_movie_tiles_content(movies))
+  # Replace the placeholder for the movie tiles with the actual dynamically generated content
+  rendered_content = main_page_content.format(movie_tiles=create_movie_tiles_content(movies))
 
-    # Output the file
-    output_file.write(main_page_head + rendered_content)
-    output_file.close()
+  # Output the file
+  output_file.write(main_page_head + rendered_content)
+  output_file.close()
 
-    # open the output file in the browser
-    url = os.path.abspath(output_file.name)
-    webbrowser.open('file://' + url, new=2)  # open in a new tab, if possible
+  # open the output file in the browser
+  url = os.path.abspath(output_file.name)
+  webbrowser.open('file://' + url, new=2) # open in a new tab, if possible
